31c0cdd152c1136d7d3a8c0f84972f554f47d062
Priyank <priyankt68@gmail.com>
2014-04-16 18:59:37 -0300

    adding mirror.cpp

--- inorder.cpp
+++ inorder.cpp
@@ -1,12 +1,15 @@
-/*
+n/*
 
 
 Tree
 					10
 
 			11			12
-		13		41
-	14		47	
+		13	
+	14
+143
+
+
 */
 
 
@@ -68,8 +71,8 @@ struct node* root = newnode(10);
        root->right = newnode(12);
        root->left->left = newnode(13);
 		root->left->left->left = newnode(14);
-		root->left->left->left->left = newnode(141);
-		root->left->left->left->left->left = newnode(143);
+	//	root->left->left->left->left = newnode(141);
+		root->left->left->left->left = newnode(143);
 
 		inorder(root);
 
