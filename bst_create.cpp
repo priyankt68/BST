5f40d175bbd62673fdef2f69d64a23f95b45ed42
Priyank <priyankt68@gmail.com>
2014-04-16 14:27:42 -0300

    adding bst_create

---
+++ bst_create.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+
+struct node
+{
+	int data;
+	struct node* left;
+	struct node* right;
+	
+};
+struct node* newnode(int);
+
+struct node* newnode(int data)
+{
+	struct node* nnode =  new node;
+
+	nnode->left = NULL;
+	nnode->right = NULL;
+	nnode->data = data;
+return nnode ;
+}
+
+int main()
+{
+
+struct node* root = newnode(10);
+       root->left = newnode(11);
+       root->right = newnode(12);
+       root->left->left = newnode(13);
+		root->left->->left->left = newnode(14);
+
+
+return 0;
+
+}
